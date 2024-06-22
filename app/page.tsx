aa36677bd8a6636e1c426ed2db1347afea5eced9
JonathanThoelke <74558743+JonathanThoelke@users.noreply.github.com>
2024-06-22 13:48:43 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pflanzen-App</h1>
-      <p color="red">Starting...</p>
+      <p color="red">START</p>
     </main>
   );
 }
