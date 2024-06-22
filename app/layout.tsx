ad36fcbed38a0a4ed5b4724ebe9071ba2eaacb56
JonathanThoelke <jonathan.thoelke@hsrw.org>
2024-06-22 19:13:59 +0200

    Test von burger-menu icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,21 +15,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="de">
       <body className={inter.className}>
         <header style={{backgroundColor: "#FDCA40", padding: "1rem"}}>
+          <img src="/burger-menu-svgrepo-com.svg" style={{width: 50,height:50}}></img>
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/Shop">Shop</a></li>
             <li><a href="/Pflanzen">Pflanzen</a></li>
-            <li>
-              <form>
-                <label>Suche:</label>
-                <input type="search"></input>
-                <input type="submit"></input>
-              </form>
-            </li>
           </ul>
+          <form>
+            <label>Suche:</label>
+            <input type="search"></input>
+            <input type="submit"></input>
+          </form>
         </header>
         <main>{ children }</main>
         <footer style={{backgroundColor: "#EEEEEE"}}>
