bf4b6eb3b599b6fd8da4f38266a733ab67ff7171
JonathanThoelke <jonathan.thoelke@hsrw.org>
2024-06-22 12:11:19 +0200

    Testing secondary remote access

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Pflanzen-App</h1>
+      <p color="red">Testing...</p>
     </main>
   );
 }
