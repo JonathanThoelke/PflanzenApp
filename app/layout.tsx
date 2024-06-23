e735dcba72a90216ceffcb5ba8b6149e6d567c87
Marit (Xanten) <20tiram03mx.de>
2024-06-24 00:45:16 +0200

    Invalid `<Link>` with `<a>` child

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,23 +15,23 @@ export default function RootLayout({
   return (
     <html lang="de">
       <body>
-        <header className="p-4 flex justify-between items-center" style={{backgroundColor: "#FDCA40"}}>
+        <header className="p-4 flex justify-between items-center" style={{ backgroundColor: "#FDCA40" }}>
           <img src="/burger-menu-svgrepo-com.svg" className="w-12 h-12" alt="Menu" />
           <nav className="flex gap-8">
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/Shop"><a>Shop</a></Link>
-            <Link href="/Kontakt"><a>Kontakt</a></Link>
-            <Link href="/Impressum"><a>Über Uns</a></Link>
+            <Link href="/">Home</Link>
+            <Link href="/Shop">Shop</Link>
+            <Link href="/Kontakt">Kontakt</Link>
+            <Link href="/Impressum">Über Uns</Link>
           </nav>
           <div className="flex items-center bg-white p-2 rounded">
             <input type="search" placeholder="Search" className="w-24 h-4 p-1" />
             <img src="/search-svgrepo-com.svg" className="w-4 h-4 inline" alt="Search" />
           </div>
-          <Link href="/"><a><img src="/Logo.png" className="w-12 h-12" alt="Logo" /></a></Link>
+          <Link href="/"><img src="/Logo.png" className="w-12 h-12" alt="Logo" /></Link>
         </header>
         <main>{children}</main>
         <footer className="bg-gray-200 p-4 text-center">
-          <Link href="/Impressum"><a>Impressum</a></Link>
+          <Link href="/Impressum">Impressum</Link>
         </footer>
       </body>
     </html>
