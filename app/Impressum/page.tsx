20f98276a529cbb694d790eb1a9442d1ddc208b0
Marit (Xanten) <20tiram03mx.de>
2024-06-24 02:35:50 +0200

    beispielimpressum hinzugefügt

--- app/Impressum/page.tsx
+++ app/Impressum/page.tsx
@@ -1,8 +1,23 @@
-//Impressum
-export default function Home() {
-    return (
-      <div>
-        <h1>Hier ist das Impressum</h1>
-      </div>
-    );
+// Impressum
+export default function Impressum() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Impressum</h1>
+      <p className="mb-4">
+        Pflanzen-App Projekt<br />
+        Straße Hausnummer<br />
+        PLZ Stadt<br />
+        Deutschland<br />
+        <br />
+        <i>E-Mail:</i> <i>info@pflanzen-app-projekt.de</i><br />
+        <br />
+        Verantwortlich für den Inhalt:<br />
+        Vorname Nachname<br />
+        Straße Hausnummer<br />
+        PLZ Ort<br />
+        <br />
+        <strong>Diese App wird im Rahmen eines studentischen Projekts entwickelt.</strong><br />
+      </p>
+    </div>
+  );
 }
