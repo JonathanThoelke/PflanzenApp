cb0a1e5c5c3a73776f8390c0667b7a2ea2617778
Marit <20tiram03@gmx.de>
2024-06-24 11:56:28 +0200

    versucht in box zu integrieren

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,10 +2,12 @@
 
 const AddToCart = () => {
   return (
-    <button onClick={() => console.log('Click')}>Zum Warenkorb hinzufügen
+    <button 
+      onClick={() => console.log('Zum Warenkorb hinzugefügt')} 
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Zum Warenkorb hinzufügen
     </button>  
   )
 }
 
-export default AddToCart
-
+export default AddToCart;
