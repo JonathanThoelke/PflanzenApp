e5bdd9afa8173bda67b293be74b407a0be7829ec
Marit <20tiram03@gmx.de>
2024-06-24 11:40:10 +0200

    einfachen Button für Kaufen hinzugefügt

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,9 +2,9 @@
 
 const AddToCart = () => {
   return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-Zum Warenkorb hinzufügen
-</button>  )
+    <button onClick={() => console.log('Click')}>Zum Warenkorb hinzufügen
+    </button>  
+  )
 }
 
 export default AddToCart
