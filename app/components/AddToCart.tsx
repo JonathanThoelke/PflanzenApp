fe781360193ae72778e07156258dfbac1f09592b
Marit <20tiram03@gmx.de>
2024-06-24 11:14:08 +0200

    Neuen Component für Warenkorb Button angelegt

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const AddToCart = () => {
+  return (
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+Zum Warenkorb hinzufügen
+</button>  )
+}
+
+export default AddToCart
+
