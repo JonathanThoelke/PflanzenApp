ec9bfdf15e1396880fe9c8d1e9cc6b5f64a9af8d
Marit <20tiram03@gmx.de>
2024-06-25 10:05:57 +0200

    Kaufen Button angepasst

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -5,7 +5,7 @@ const AddToCart = () => {
     <button 
       onClick={() => console.log('Zum Warenkorb hinzugefügt')} 
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Zum Warenkorb hinzufügen
+      kaufen
     </button>  
   )
 }
