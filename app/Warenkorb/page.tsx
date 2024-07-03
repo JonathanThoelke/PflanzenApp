9ac711840d66c49883280b52ff05826125fe91bd
Marit <20tiram03@gmx.de>
2024-07-03 23:03:22 +0200

    Warenkorb Seite aufgebaut (Grundstruktur)

--- app/Warenkorb/page.tsx
+++ app/Warenkorb/page.tsx
@@ -2,8 +2,13 @@
 
 export default function Home() {
     return (
-      <div>
-        <h1>Hier ist der Warenkorb</h1>
- </div>
+        <div className="flex justify-center items-center h-screen">
+            <div className="bg-black p-4 rounded text-white"
+                style={{ width: '80%', margin: 'auto', marginTop: '20px', borderRadius: '8px' }}>
+                <div className="flex justify-center items-center" style={{ height: '100%' }}>
+                    <span className="text-xl">Warenkorb</span>
+                </div>
+            </div>
+        </div>
     );
 }
