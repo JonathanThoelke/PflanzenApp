aa372b7efe2698791c4cc14cdbccdab58f2df846
LingYi0077 <lingyi0588@gmail.com>
2024-07-03 07:12:08 +0200

    add image

--- app/Ueber Uns/page.tsx
+++ app/Ueber Uns/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
       <div>
         <h1>Hier ist Über uns</h1>
-        <a href=""></a>
+        <a href="pflanzenapp/public/images/rose.jpg"></a>
         <p></p>
         <h2></h2>
         <p></p>
