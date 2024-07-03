61a95aff5fee34cbaf8af1ec3c81a8c7d21345f1
LingYi0077 <lingyi0588@gmail.com>
2024-07-03 15:10:06 +0200

    mit anderem Bild hinzugefügt

--- app/UeberUns/page.tsx
+++ app/UeberUns/page.tsx
@@ -6,7 +6,7 @@ const Page = () => {
   return (
     <div>
       <b>Über uns</b>
-      <img src="pflanzenapp/public/images/rose.jpg"/>
+      <img src="pflanzenapp/public/images/dahlien.png"/>
       <p>Willkommen in unserer Gartenapp!</p>
       <p>
         Wir freuen uns, dass Sie den Weg zu uns gefunden haben und
