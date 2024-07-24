5b3cab4e1b014e97a965355a4aa153498abd09be
JonathanThoelke <jonathan.thoelke@hsrw.org>
2024-07-24 18:35:33 +0200

    leere Suche angepasst

--- app/Pflanzen/page.tsx
+++ app/Pflanzen/page.tsx
@@ -14,7 +14,7 @@ const Pflanzen: React.FC = () => {
     //mit z.B. '&haustier=false' für Suchbedingungen für nicht-Textbasierte Suche
     //oder Suche nach 'NameDE', 'NameLatein' etc könnten separiert werden.
     
-    let exclusive = !!searchParams.get('exc') || false;
+    let exclusive = (searchParams.get('exc') !== 'false');
     let nameDE = searchParams.get('name');
     let nameLT = searchParams.get('nameL');
     let desc = searchParams.get('desc');
@@ -158,6 +158,11 @@ const Pflanzen: React.FC = () => {
     return (
         <div className="p-4">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+                {plants.map((plant) => (
+                    <div key={plant.ID} className="flex justify-center">
+                        <PlantCard plant={plant} />
+                    </div>
+                ))}
                 {plants.map((plant) => (
                     <div key={plant.ID} className="flex justify-center">
                         <PlantCard plant={plant} />
