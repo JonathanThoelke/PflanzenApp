d541e135eaab201b1b14bb9c5cf03a995c804a0d
Marit <20tiram03@gmx.de>
2024-07-25 12:35:58 +0200

    sidebar durch klicken außerhalb eingeklappt

--- app/components/SidebarButton.tsx
+++ app/components/SidebarButton.tsx
@@ -1,15 +1,15 @@
 'use client';
 //
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 
 const SidebarButton: React.FC = () => {
-
-    const [isFoldedOut, foldOut] = useState(true);
+    const [isFoldedOut, setFoldedOut] = useState(false);
+    const buttonRef = useRef<HTMLButtonElement>(null);
 
     const handleButtonClick = () => {
-        console.log("Hello World!")
-        foldOut(!isFoldedOut);
+        console.log("Menü geklickt")
+        setFoldedOut(!isFoldedOut);
         var targetElement = document.getElementById('sidebar');
         if(targetElement != null){
             if (targetElement.classList.contains('-left-96')) {
@@ -22,10 +22,32 @@ const SidebarButton: React.FC = () => {
         }
     };
 
+    const handleClickOutside = (event: MouseEvent) => {
+        const sidebarElement = document.getElementById('sidebar');
+        if (sidebarElement && !sidebarElement.contains(event.target as Node) && !buttonRef.current?.contains(event.target as Node)) {
+            setFoldedOut(false);
+            sidebarElement.classList.remove('left-0');
+            sidebarElement.classList.add('-left-96');
+        }
+    };
+
+    useEffect(() => {
+        if (isFoldedOut) {
+            document.addEventListener('click', handleClickOutside);
+        } else {
+            document.removeEventListener('click', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('click', handleClickOutside);
+        };
+    }, [isFoldedOut]);
+
     return (
         <button
             id="changeStyleButton"
             onClick={handleButtonClick}
+            ref={buttonRef}
         >
             <img src="menu.jpeg" className="h-12 w-8"></img>
         </button>
